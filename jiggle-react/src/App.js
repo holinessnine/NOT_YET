a3bc29db3880ac0bbb9f1ba4d50b04f0a2537a8c
holinessnine <seg3981@gmail.com>
2024-05-14 20:34:14 +0900

    리포지토리 이름 변경 테스트

--- jiggle-react/src/App.js
+++ jiggle-react/src/App.js
@@ -106,7 +106,7 @@ const App = () => {
           <div className="card mb-3">
             <img src="https://via.placeholder.com/150" className="img-fluid rounded-start" alt="..."/>
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">Card title5</h5>
               <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
             </div>
